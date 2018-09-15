31684923fdbd710f55b9ed122313d5f3765cc12a
Martin Horciak <martinhorciak@martins-macbook-pro.local>
2018-09-15 17:02:46 -0400

    Initial Commit

---
+++ Horciak_Martin_Word_Scrambler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horciak_Martin_Word_Scrambler
+//
+//  Created by Martin Horciak on 2018-09-15.
+//  Copyright © 2018 Martin Horciak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
